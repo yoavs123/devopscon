f1682ecf32af4899df85fe0e8802a25f62759a8f
yoavs123 <95212503+yoavs123@users.noreply.github.com>
2021-11-29 07:27:58 -0800

    Update HelloHandler.java

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hi there, is it working from your laptop?";
+		String hello = "Hi there, is it working from your laptop? - change it!";
 		return hello;
 	}
 
