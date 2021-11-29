f9b2fd0cc194b087584e3c9cf2a527b91542b5c5
user <user@gmail.com>
2021-11-29 07:32:14 -0800

    add changes from git terminal

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hi there, is it working from your laptop? - change it!";
+		String hello = "Hi there, is it working from your laptop? - change it another time (from terminal)!";
 		return hello;
 	}
 
